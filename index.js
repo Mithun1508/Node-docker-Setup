f9d48413829529350a7ff5d5b30e68f23bcb2692
Mithun <93249038+Mithun1508@users.noreply.github.com>
2023-01-27 10:44:51 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,34 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+var data = [];
+
+app.post("/webhook", async function (req, res) {
+  console.log(req.body);
+  data.push(req.body);
+  res.send({ ack: "true", currentTime: new Date().toISOString() });
+});
+
+app.get("/webhook/check", async function (req, res) {
+  res.send(data);
+});
+
+app.get("/webhook/clear", async function (req, res) {
+  data = [];
+  res.send(data);
+});
+
+app.post("/webhook2", async function (req, res) {
+  console.log(req.body);
+  res.send({ ack: "true", currentTime: new Date().getTime().toString() });
+});
+
+function getTime(date) {
+  return new Date(date).getTime().toString();
+}
+
+var server = app.listen(3000, function () {});
+console.log("Server is Started !!!");
